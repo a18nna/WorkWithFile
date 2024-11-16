f68c96979b035dbdf1f46007e43cff31ab8b01b1
al18nna <melnik.alena2006@gmail.com>
2024-11-16 13:22:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите путь к папке: ");
+            string fullPath = Path.GetFullPath(Console.ReadLine());
+
+            FolderCleaning(fullPath);
+
+            Console.ReadKey();
+        }
+
+        public static void FolderCleaning(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                TimeSpan LastAccess = TimeSpan.FromMinutes(30);
+
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    if ((Directory.GetLastAccessTime(dir)).Add(LastAccess) < DateTime.Now)
+                    {
+                        try
+                        {
+                            Directory.Delete(dir, true);
+                            Console.WriteLine($"Каталог {dir} удален.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                {
+                    if ((File.GetLastAccessTime(file)).Add(LastAccess) < DateTime.Now)
+                    {
+                        try
+                        {
+                            File.Delete(file);
+                            Console.WriteLine($"Файл {file} удален.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлу: ");
+
+            string fullPath = Path.GetFullPath(Console.ReadLine());
+
+            try
+            {
+                if (Directory.Exists(fullPath))
+                {
+                    GetDirectorySize(fullPath);
+                }
+                else
+                {
+                    Console.WriteLine($"Директория по пути '{fullPath}' не существует.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            Console.ReadKey();
+        }
+
+        public static void GetDirectorySize(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine($"Директория по пути '{path}' не существует.");
+                return;
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            long totalSize = dir.EnumerateFiles("*.*", SearchOption.AllDirectories).Sum(fi => fi.Length);
+            Console.WriteLine($"Размер каталога '{path}': {totalSize} байт");
+        }
+    }
+}
